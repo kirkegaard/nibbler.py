75540d1401b0e5b717ae4609edf57e6399196263
Christian Kirkegaard <hello@christiank.org>
2017-07-05 17:27:07 +0200

    first init

---
+++ bot.py
@@ -0,0 +1,93 @@
+from discord.ext import commands
+import datetime
+import json, asyncio
+import logging
+import sys
+
+try:
+    import uvloop
+except ImportError:
+    pass
+else:
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+description = """
+Hello! Do you remember some months ago when the Earth was under attack by flying brains?
+"""
+
+extensions = [
+    'extensions.rng',
+    'extensions.purge',
+    'extensions.member',
+    'extensions.moderator'
+]
+
+# silence discord.py
+discord_logger = logging.getLogger('discord')
+discord_logger.setLevel(logging.CRITICAL)
+
+handler = logging.FileHandler(filename='nibbler.log', encoding='utf-8', mode='w')
+logging.basicConfig(format='%(asctime)-15s %(message)s')
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+log.addHandler(handler)
+
+help_attrs = dict(hidden=True)
+
+prefix = ['!']
+bot = commands.Bot(command_prefix=prefix, description=description, pm_help=None, help_attrs=help_attrs)
+
+
+@bot.event
+async def on_ready():
+    print('Ready Eddy!')
+
+@bot.command()
+async def load(extension_name : str):
+    """Loads an extension."""
+    try:
+        bot.load_extension(extension_name)
+    except (AttributeError, ImportError) as e:
+        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
+        return
+    await bot.say("{} loaded.".format(extension_name))
+
+@bot.command()
+async def unload(extension_name : str):
+    """Unloads an extension."""
+    bot.unload_extension(extension_name)
+    await bot.say("{} unloaded.".format(extension_name))
+
+@bot.command()
+async def add(left : int, right : int):
+    """Adds two numbers together."""
+    await bot.say(left + right)
+
+@bot.event
+async def on_message(message):
+    if message.author.bot:
+        return
+
+    await bot.process_commands(message)
+
+
+if __name__ == '__main__':
+    with open('credentials.json') as creds:
+        credentials = json.load(creds)
+
+    debug = any('debug' in arg.lower() for arg in sys.argv)
+    token = credentials['token']
+    bot.owner_id = credentials['owner_id']
+
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+            print('Loaded extension: {}'.format(extension))
+        except Exception as e:
+            print('Failed to load extension {}\n{}: {}'.format(extension, type(e).__name__, e))
+
+    bot.run(token)
+    handlers = log.handlers[:]
+    for hdlr in handlers:
+        hdlr.close()
+        log.removeHandler(hdlr)
---
+++ extensions/moderator.py
@@ -0,0 +1,47 @@
+import discord
+from discord.ext import commands
+import logging
+
+log = logging.getLogger(__name__)
+
+class Moderator():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(no_pm=True)
+    async def kick(self, *, member : discord.Member):
+        try:
+            await self.bot.kick(member)
+        except discord.Forbidden:
+            await self.bot.say('The bot does not have permissions to kick this member.')
+        except discord.HTTPException:
+            await self.bot.say('Kicking failed.')
+        else:
+            await self.bot.say('\U0001f44c')
+
+    @commands.command(no_pm=True)
+    async def ban(self, *, member : discord.Member):
+        try:
+            await self.bot.ban(member)
+        except discord.Forbidden:
+            await self.bot.say('The bot does not have permissions to ban this member.')
+        except discord.HTTPException:
+            await self.bot.say('Banning failed.')
+        else:
+            await self.bot.say('\U0001f44c')
+
+    # This is kind of wonky since it requires the users id
+    # We need to figure out a way to find the user easier
+    @commands.command(pass_context=True, no_pm=True)
+    async def unban(self, context, member_id):
+        try:
+            await self.bot.unban(context.message.server, discord.Object(member_id))
+        except discord.Forbidden:
+            await self.bot.say('The bot does not have permissions to unban this member.')
+        except discord.HTTPException:
+            await self.bot.say('Unbanning failed.')
+        else:
+            await self.bot.say('\U0001f44c')
+
+def setup(bot):
+    bot.add_cog(Moderator(bot))
