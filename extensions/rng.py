4365df30c3f92d93d9687590be472dba87d2b221
Christian Kirkegaard <hello@christiank.org>
2017-07-20 00:56:01 +0200

    8ball

--- extensions/rng.py
+++ extensions/rng.py
@@ -1,9 +1,36 @@
 import random
 from discord.ext import commands
 
+EIGHTBALL = [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don\'t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'
+];
+
 class RNG():
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(name='8ball')
+    async def eightball(self):
+        await self.bot.say(random.choice(EIGHTBALL))
 
     @commands.command()
     async def roll(self, dice : str):
