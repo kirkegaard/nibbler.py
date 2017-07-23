9dd92c626d82be979d7e8d6036be62091b5627c0
Christian Kirkegaard <hello@christiank.org>
2017-07-23 23:29:24 +0200

    add imdb search

--- extensions/search.py
+++ extensions/search.py
@@ -2,6 +2,7 @@ import discord
 import requests
 import google
 from discord.ext import commands
+from imdbpie import Imdb
 
 
 class Search():
@@ -20,6 +21,23 @@ class Search():
         """Searches google video (youtube) and returns the first result"""
         result = google.search_videos(' '.join(query), num=1)
         await context.channel.send(next(result))
+
+    @commands.command()
+    async def imdb(self, context, *query: str):
+        """Searches imdb for a movie title"""
+        imdb = Imdb(anonymize=True)
+        search = imdb.search_for_title(query)
+        res = imdb.get_title_by_id(search[0]['imdb_id'])
+
+        msg = discord.Embed(
+            colour=0xffff00,
+            title='{} ({})'.format(res.title, res.year),
+            description=res.plot_outline,
+            url='https://imdb.com/title/{}'.format(res.imdb_id)
+        )
+        msg.set_image(url=res.poster_url)
+
+        await context.channel.send(embed=msg)
 
 
 def setup(bot):
