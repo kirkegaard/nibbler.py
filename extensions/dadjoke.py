07d55d52f30a6be614a2a719e25b90cc2af9d0a3
Christian Kirkegaard <hello@christiank.org>
2017-07-28 22:02:29 +0200

    add dad joke extension

---
+++ extensions/dadjoke.py
@@ -0,0 +1,20 @@
+import requests
+import pprint
+from discord.ext import commands
+
+
+class Dadjoke():
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def dadjoke(self, context):
+        """Tells a dad joke"""
+        headers = {"Accept": "text/plain"}
+        res = requests.get('https://icanhazdadjoke.com', headers=headers)
+        await context.send(res.text)
+
+
+def setup(bot):
+    bot.add_cog(Dadjoke(bot))
