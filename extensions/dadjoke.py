a20b24d7075fbd72ffe6e4b05fff173b0cae41e1
Christian Kirkegaard <hello@christiank.org>
2017-07-31 02:40:00 +0200

    Added a notification extension

--- extensions/dadjoke.py
+++ extensions/dadjoke.py
@@ -1,5 +1,4 @@
 import requests
-import pprint
 from discord.ext import commands
 
 
--- utils/config.py
+++ utils/config.py
@@ -4,6 +4,15 @@ import uuid
 import asyncio
 
 
+def _create_encoder(cls):
+    def _default(self, o):
+        if isinstance(o, cls):
+            return o.to_json()
+        return super().default(o)
+
+    return type('_Encoder', (json.JSONEncoder,), {'default': _default})
+
+
 class Config:
     """The "database" object. Internally based on ``json``."""
 
@@ -11,6 +20,15 @@ class Config:
         self.name = name
         self.object_hook = options.pop('object_hook', None)
         self.encoder = options.pop('encoder', None)
+
+        try:
+            hook = options.pop('hook')
+        except KeyError:
+            pass
+        else:
+            self.object_hook = hook.from_json
+            self.encoder = _create_encoder(hook)
+
         self.loop = options.pop('loop', asyncio.get_event_loop())
         self.lock = asyncio.Lock()
         if options.pop('load_later', False):
@@ -30,7 +48,7 @@ class Config:
             await self.loop.run_in_executor(None, self.load_from_file)
 
     def _dump(self):
-        temp = '%s-%s.tmp' % (uuid.uuid4(), self.name)
+        temp = '%s-%s' % (self.name, uuid.uuid4())
         with open(temp, 'w', encoding='utf-8') as tmp:
             json.dump(self._db.copy(), tmp, ensure_ascii=True,
                       cls=self.encoder, separators=(',', ':'))
@@ -44,23 +62,23 @@ class Config:
 
     def get(self, key, *args):
         """Retrieves a config entry."""
-        return self._db.get(key, *args)
+        return self._db.get(str(key), *args)
 
     async def put(self, key, value, *args):
         """Edits a config entry."""
-        self._db[key] = value
+        self._db[str(key)] = value
         await self.save()
 
     async def remove(self, key):
         """Removes a config entry."""
-        del self._db[key]
+        del self._db[str(key)]
         await self.save()
 
     def __contains__(self, item):
-        return item in self._db
+        return str(item) in self._db
 
     def __getitem__(self, item):
-        return self._db[item]
+        return self._db[str(item)]
 
     def __len__(self):
         return len(self._db)
