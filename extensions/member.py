b8d006134e8eae4b311d17cc0d0dfc9e102bba19
kirkegaard <hello@christiank.org>
2017-11-20 13:11:36 +0100

    default channel is the first channel from the channels list

--- extensions/member.py
+++ extensions/member.py
@@ -13,12 +13,12 @@ class Member():
 
     async def on_member_join(self, member: discord.Member):
         msg = 'Velkommen {0.mention}. Husk at læse vores regler og nyheder i kanalen til venstre. Husk også at give dig selv til kende med dit BM-brugernavn, så vi ved, hvem du er. Ohøj!'
-        await member.guild.default_channel.send(msg.format(member))
+        await member.guild.channels[0].send(msg.format(member))
         log.info('[member.join] {0.name} {0.mention}'.format(member))
 
     async def on_member_remove(self, member: discord.Member):
         msg = 'Farvel {0.mention}... TTYN!'
-        await member.guild.default_channel.send(msg.format(member))
+        await member.guild.channels[0].send(msg.format(member))
         log.info('[member.remove] {0.name} {0.mention}'.format(member))
 
 
