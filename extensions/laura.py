f52f90bbd2cf802b57b017ecc9f07dec9f90879e
Christian Kirkegaard <hello@christiank.org>
2017-12-06 01:23:01 +0100

    bump

--- extensions/laura.py
+++ extensions/laura.py
@@ -9,21 +9,15 @@ class Laura():
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_message(self, message):
-        if message.author.id in laura_id and sum(1 for c in message.content if c.isupper()) > 10:
-            await message.channel.send('Dæmp dig Laura')
+    async def on_message(self, ctx):
+        # if message.author.id in laura_id and sum(1 for c in message.content if c.isupper()) > 10:
+        #     await message.channel.send('Dæmp dig Laura')
 
         rand = randint(0, 1000)
         msg = None
 
-        if rand == 69:
-            msg = 'Laura er sød <3'
-
-        if rand == 42:
-            msg = 'Hold kæft Laura!'
-
-        if msg is not None:
-            await message.channel.send(msg)
+        if ctx.author.id in laura_id and rand > 800:
+            await ctx.add_reaction('\U0001F4A9')
 
 
 def setup(bot):
