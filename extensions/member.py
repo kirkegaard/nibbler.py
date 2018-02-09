a4fc73b5a55d84fe60f6d09550111b4a32b5e2b8
Christian Kirkegaard <hello@christiank.org>
2018-02-09 21:00:25 +0100

    fix member

--- extensions/member.py
+++ extensions/member.py
@@ -5,6 +5,17 @@ from discord.ext import commands
 
 log = logging.getLogger(__name__)
 
+MESSAGES = {
+    260858852776476672: [
+        'Velkommen {0.mention}. Husk at læse vores regler og nyheder i kanalen til venstre. Husk også at give dig selv til kende med dit BM-brugernavn, så vi ved, hvem du er. Ohøj!',
+        'Farvel {0.mention}... TTYN!'
+    ],
+    312288797197074432: [
+        'Hva så?',
+        'Sæs'
+    ]
+}
+
 
 class Member():
 
@@ -12,14 +23,16 @@ class Member():
         self.bot = bot
 
     async def on_member_join(self, member: discord.Member):
-        msg = 'Velkommen {0.mention}. Husk at læse vores regler og nyheder i kanalen til venstre. Husk også at give dig selv til kende med dit BM-brugernavn, så vi ved, hvem du er. Ohøj!'
-        await member.guild.channels[0].send(msg.format(member))
-        log.info('[member.join] {0.name} {0.mention}'.format(member))
+        if member.guild.id in MESSAGES:
+            msg = MESSAGES[member.guild.id][0].format(member)
+            await member.guild.channels[0].send(msg)
+            log.info('[member.join] {0.name} {0.mention}'.format(member))
 
     async def on_member_remove(self, member: discord.Member):
-        msg = 'Farvel {0.mention}... TTYN!'
-        await member.guild.channels[0].send(msg.format(member))
-        log.info('[member.remove] {0.name} {0.mention}'.format(member))
+        if member.guild.id in MESSAGES:
+            msg = MESSAGES[member.guild.id][1].format(member)
+            await member.guild.channels[0].send(msg)
+            log.info('[member.join] {0.name} {0.mention}'.format(member))
 
 
 def setup(bot):
