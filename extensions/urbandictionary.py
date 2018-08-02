81f94975be40dd8743dea1ab69edaafb54c3311b
Christian Kirkegaard <hello@christiank.org>
2018-08-03 00:12:45 +0200

    Fix urban dict

--- extensions/urbandictionary.py
+++ extensions/urbandictionary.py
@@ -1,18 +1,25 @@
-import urbandictionary as ud
+# import urbandictionary as ud
+import requests
 import discord
 from discord.ext import commands
 
+
 class UrbanDict():
+
+    endpoint = 'http://api.urbandictionary.com/v0/define?term=%s'
 
     def __init__(self, bot):
         pass
 
     @commands.command(aliases=['ud'])
-    async def urbandict(self, context, *subject: str):
-        defs = ud.define(' '.join(subject))
-        if defs:
-            await context.channel.send(defs[0].definition)
+    async def urbandict(self, context, *word: str):
+        word = ' '.join(word)
+        res = requests.get(self.endpoint % word)
+        definition = res.json()['list'][0]
+        print(definition)
+
+        await context.channel.send('**{}**: {}\n**Example**: {}'.format(definition['word'], definition['definition'], definition['example']))
+
 
 def setup(bot):
     bot.add_cog(UrbanDict(bot))
-
