85bf61e5bf00c0650d6ec5c4976a37e989b4740c
Christian Kirkegaard <hello@christiank.org>
2018-08-02 22:59:35 +0200

    fix name confligt

--- extensions/subscriptions.py
+++ extensions/subscriptions.py
@@ -2,8 +2,8 @@ import sqlite3
 from discord.ext import commands
 from utils import config
 
-config = config.Config('config/bot.json')
-ROLES = config.get('roles')
+conf = config.Config('config/bot.json')
+ROLES = conf.get('roles')
 
 
 class Subscriptions():
