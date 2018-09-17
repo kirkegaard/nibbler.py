d35ac444ba78bbc782e33240bef5febebcee9f40
Christian Kirkegaard <christian@lowpoly.dk>
2018-09-17 20:55:51 +0200

    bump

--- extensions/twitch.py
+++ extensions/twitch.py
@@ -11,6 +11,8 @@ class Twitch():
     def __init__(self, bot):
         self.bot = bot
         self.config = config.Config('config/bot.json')
+        self.callback = '{}/callback'.format(self.config.get('bot_host'))
+        self.headers = {"Client-ID": self.config.get('twitch_id')}
 
     @commands.group()
     async def twitch(self, context):
@@ -18,20 +20,36 @@ class Twitch():
             await context.send('Use `!twitch sub [user]` to subscribe to user. Or use `!twitch unsub [user]` to remove user.')
 
     @twitch.command(alias=['sub'])
-    async def add(self, context):
-        # https://api.twitch.tv/helix/webhooks/hub?hub.mode=subscribe&hub.topic=https://api.twitch.tv/helix/streams?user_id={user_id}
-        pass
+    async def add(self, context, username):
+        endpoint = 'https://api.twitch.tv/helix/webhooks/hub'
+        topic = 'https://api.twitch.tv/helix/streams'
+
+        user_id = self.get_user_id(username)
+        if not user_id:
+            return False
+
+        payload = {
+            'hub.callback': self.callback,
+            'hub.mode': 'subscribe',
+            'hub.topic': '{}?user_id={}'.format(topic, user_id),
+        }
+        res = requests.post(endpoint, params=payload, headers=self.headers)
+        print(self.callback, user_id, payload, res)
 
     @twitch.command(alias=['unsub', 'del'])
     async def delete(self, context):
         pass
 
     def get_user_id(self, username):
-        pass
+        endpoint = 'https://api.twitch.tv/helix/users?login={}'.format(username)
+        res = requests.get(endpoint, headers=self.headers).json()
+        if not res['data']:
+            return False
+        return res['data'][0]['id']
 
-    async def post(self, request):
-        res = request.json
-        print(res)
+    async def callback(self, request):
+        print(request)
+        return json({}, status=200)
         # if request.headers['Authorization'] == self.config.get('bot_auth'):
         #     channel = self.bot.get_channel(res['channel'])
         #     await channel.send(res['msg'])
@@ -42,4 +60,4 @@ class Twitch():
 def setup(bot):
     twitch = Twitch(bot)
     bot.add_cog(twitch)
-    bot.api.add_route(twitch.post, '/twitch', methods=['POST'])
+    bot.api.add_route(twitch.callback, '/callback', methods=['GET'])
