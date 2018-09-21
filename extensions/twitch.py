e2c87d0356eb6b6c69d85536affaae651fb66d1d
Christian Kirkegaard <hello@christiank.org>
2018-09-21 14:29:36 +0200

    add list and unsub method

--- extensions/twitch.py
+++ extensions/twitch.py
@@ -13,8 +13,6 @@ class Twitch():
         self.config = config.Config('config/bot.json')
         self.headers = {"Client-ID": self.config.get('twitch_id')}
         self.api = 'https://api.twitch.tv/helix'
-        #self.channel = 482186488340283402
-        self.channel = 483727348596998144
 
     @commands.group()
     async def twitch(self, context):
@@ -30,8 +28,8 @@ class Twitch():
             return await context.send('{} is not streaming'.format(username))
         return await context.send('{} is currently streaming'.format(username))
 
-    @twitch.command(alias=['sub'])
-    async def add(self, context, username):
+    @twitch.command()
+    async def sub(self, context, username):
         endpoint = '%s/webhooks/hub' % self.api
         topic = '%s/streams' % self.api
 
@@ -39,19 +37,62 @@ class Twitch():
         if not user_id:
             return False
 
+        channel = '{}/callback?channel={}'.format(self.config.get('bot_host'), context.channel.id)
         payload = {
-            'hub.callback': '{}/callback'.format(self.config.get('bot_host')),
+            'hub.callback': channel,
             'hub.mode': 'subscribe',
             'hub.lease_seconds': 864000,
             'hub.topic': '{}?user_id={}'.format(topic, user_id),
         }
-        res = requests.post(endpoint, params=payload, headers=self.headers)
-        print(res.status_code)
-        await context.send('{} added'.format(username))
 
-    @twitch.command(alias=['unsub', 'del'])
-    async def delete(self, context):
-        pass
+        res = requests.post(endpoint, params=payload, headers=self.headers)
+        if res.status_code == 202:
+            await context.send('{} added'.format(username))
+
+    @twitch.command()
+    async def list(self, context):
+        subscriptions = self.get_subscriptions()
+        if not subscriptions:
+            return False
+
+        for k, v in enumerate(subscriptions['data']):
+            topic = self.parse_url(v['topic'])
+            user = self.get_user(topic['user_id'])
+            username = user['display_name']
+
+            callback = self.parse_url(v['callback'])
+            channel = callback['channel']
+
+            await context.send('[{}] {} : {}'.format(k, username, channel))
+
+    @twitch.command()
+    async def unsub(self, context, idx: int):
+        endpoint = '%s/webhooks/hub' % self.api
+        subscriptions = self.get_subscriptions()
+        if not subscriptions:
+            return False
+
+        subscription = subscriptions['data'][idx]
+        payload = {
+            'hub.mode': 'unsubscribe',
+            'hub.callback': subscription['callback'],
+            'hub.topic': subscription['topic']
+        }
+        res = requests.post(endpoint, params=payload, headers=self.headers)
+        if res.status_code == 202:
+            await context.send('Subscription removed')
+
+    def get_subscriptions(self):
+        endpoint = '%s/webhooks/subscriptions' % self.api
+        access_token = self.get_access_token()
+        headers = {'Authorization': 'Bearer {}'.format(access_token)}
+        res = requests.get(endpoint, headers=headers).json()
+        return res
+
+    def parse_url(self, url):
+        query = requests.utils.urlparse(url).query
+        params = dict(x.split('=') for x in query.split('&'))
+        return params
 
     def get_user(self, user_id):
         endpoint = '{}/users?id={}'.format(self.api, user_id)
@@ -67,18 +108,35 @@ class Twitch():
             return False
         return res['data'][0]['id']
 
+    def get_access_token(self):
+        endpoint = 'https://id.twitch.tv/oauth2/token'
+        payload = {
+            'client_id': self.config.get('twitch_id'),
+            'client_secret': self.config.get('twitch_secret'),
+            'grant_type': 'client_credentials'
+        }
+        res = requests.post(endpoint, params=payload).json()
+        return res['access_token']
+
     async def callback(self, request):
         challenge = request.args.get('hub.challenge')
         return response.text(challenge, status=200)
 
     async def handle(self, request):
-        data = request.json['data']
+        data = request.json.get('data')
         if not data:
-            return False
+            return response.text('', status=200)
+
         user_id = data[0]['user_id']
-        channel = self.bot.get_channel(self.channel)
         user = self.get_user(user_id)
-        await channel.send('{u[display_name]} is streaming at https://twitch.tv/{u[login]}'.format(u=user))
+
+        channel_id = int(request.args.get('channel'))
+        channel = self.bot.get_channel(channel_id)
+
+        await channel.send('{display_name} is streaming at https://twitch.tv/{login}'.format(
+            display_name=user['display_name'],
+            login=user['login']
+        ))
         return response.text('', status=200)
 
 
