fb94667153a8161cac8c479e7ad486101f6f57c8
Christian Kirkegaard <christian@lowpoly.dk>
2018-09-26 22:08:56 +0200

    Add extension thatll reply to specific words

---
+++ extensions/reply.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+from utils import config
+import random
+
+REPLYS = {
+    'godnat': [
+        'Godnat ven :*',
+        'Sov godt søtte',
+        'Drøm om mig <3'
+    ],
+    'goddag': [
+        'Hej ven <3',
+        'Hvordan har du det?',
+        'Godt at se dig søtte!',
+        'Du ser godt ud i dag :D'
+    ]
+}
+
+
+class Reply():
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, context):
+        first = context.content.split(' ')[0]
+        if first in REPLYS:
+            reply = random.choice(REPLYS[first])
+            await context.channel.send(reply)
+
+def setup(bot):
+    reply = Reply(bot)
+    bot.add_cog(reply)
