84a0eae244f32f8cb09308fdd56aef984475940c
Christian Kirkegaard <christian@lowpoly.dk>
2019-04-27 20:08:17 +0200

    flaf

--- extensions/reddit.py
+++ extensions/reddit.py
@@ -6,24 +6,9 @@ import json
 
 from discord.ext import commands
 
-SUBREDDITS_PORN = [
-    'nsfw',
-    'Amateur',
-    'Blowjobs',
-    'GirlsFinishingTheJob',
-    'NSFW_GIF',
-    'creampies',
-    'cumsluts',
-    'dirtysmall',
-    'facesitting',
-    'nsfw_gifs',
-    'nsfwhardcore',
-    'omgbeckylookathiscock',
-    'porn_gifs'
-]
-
 headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+    'user-agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+}
 
 
 class Reddit():
@@ -32,30 +17,25 @@ class Reddit():
         self.bot = bot
 
     @commands.command()
-    async def porn(self, context, count=1):
-        """Fetches a random picture from subreddits. *NSFW channels only"""
-        if not context.message.channel.is_nsfw():
-            await context.send("This isnt a nsfw channel")
-            return
-
+    async def reddit(self, context, subreddits='', count=1):
+        """Fetches a random picture from subreddits."""
         if count > 10:
             await context.send("I can only fetch 10 images. Sorry :(")
             return
 
         res = []
         for x in range(1, count):
-            endpoint = 'http://reddit.com/r/{}/random/.json'.format(
-                random.choice(SUBREDDITS_PORN))
+            endpoint = 'http://reddit.com/r/{}/random/.json'.format(subreddits)
 
             with requests_cache.disabled():
                 data = requests.get(endpoint, headers=headers).json()
+                print(data)
                 link = data[0]["data"]["children"][0]["data"]["url"]
                 if not link in res:
                     res.append(link)
 
         for y in res:
             await context.send(y)
-
 
 def setup(bot):
     bot.add_cog(Reddit(bot))
